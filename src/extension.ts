69812722f6dfb870a647f89210cf4973d430fc8a
bluebear <347278372@qq.com>
2023-11-26 01:37:00 +0800

    添加了创建终端执行命令功能

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,120 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "cmdterminalmaker" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('cmdterminalmaker.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CmdTerminalMaker!');
-	});
-
-	context.subscriptions.push(disposable);
+interface Parameter {
+	name: string;
+	default?: string;
+	placeHolder?: string;
+	chosen?: vscode.QuickPickItem[];
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+class Command {
+	terminalName?: string;
+	command: string = '';
+	name: string = '';
+	parameter?: { defalut?: string; placeHolder?: string; } | Parameter[];
+
+	constructor(name: string, command: string, terminalName?: string, parameter?: { defalut?: string; placeHolder?: string; } | Parameter[]) {
+		this.name = name;
+		this.command = command;
+		this.terminalName = terminalName;
+		this.parameter = parameter;
+	}
+
+	async execute() {
+		let terminal = vscode.window.createTerminal(this.terminalName ?? this.name);
+		const parameters: string[] = await this.getParameter(this);
+		terminal.sendText(`${this.command} ${parameters.join(' ')}`);
+		terminal.show();
+	}
+
+	async getParameter(command: Command): Promise<string[]> {
+		if (command.parameter === undefined) return [];
+		let parameters: string[] = [];
+		if (this.parameter instanceof Array) {
+			for (let parameter of this.parameter) {
+				let value = undefined;
+				if (parameter.chosen?.length) {
+					value = (await vscode.window.showQuickPick(parameter.chosen, {
+						placeHolder: parameter.placeHolder,
+						matchOnDescription: true,
+						ignoreFocusOut: true,
+					}))?.label;
+				} else {
+					value = await vscode.window.showInputBox({
+						prompt: `为命令 ${command.command} ${parameters.join(' ')} ${parameters.length > 0 ? '继续' : ''}输入参数 ${parameter.name ?? ''} `,
+						placeHolder: parameter.placeHolder,
+						value: parameter.default,
+						ignoreFocusOut: true,
+					});
+				}
+				if (value === undefined) throw new Error('用户取消输入');
+				parameters.push(value.trim());
+			}
+		} else {
+			const value = await vscode.window.showInputBox({
+				prompt: `为命令 ${command.command} 一次性输入所有参数 用' '分割`,
+				placeHolder: this.parameter?.placeHolder,
+				value: this.parameter?.defalut,
+				ignoreFocusOut: true,
+			});
+			if (value === undefined) throw new Error('用户取消输入');
+			return value.trim().split(' ');
+		}
+		return parameters;
+	}
+
+}
+
+async function executeCommands(commands: Command[]) {
+	if (commands.length === 0) return;
+	let progressOptions = {
+		location: vscode.ProgressLocation.Notification,
+		cancellable: true
+	};
+
+	vscode.window.withProgress(progressOptions, async (progress, token) => {
+		return new Promise<void>(async (resolve, reject) => {
+			for (let i = 0; i < commands.length; i++) {
+				if (token.isCancellationRequested) {
+					if (i > 0) vscode.window.showInformationMessage(`已经执行了以下命令：${commands.slice(0, i).map(cmd => cmd.name).join(', ')}`);
+					vscode.window.showWarningMessage(`以下命令没有执行：${commands.slice(i).map(cmd => cmd.name).join(', ')}`);
+					reject();
+					return;
+				}
+				try {
+					progress.report({ increment: 100 / commands.length, message: `当前命令${commands[i].name}, 进度(${i + 1}/${commands.length})` });
+					await commands[i].execute();
+				} catch (error) {
+					vscode.window.showErrorMessage(`执行命令${commands[i].name}时出错：${error}`);
+					if (i > 0) vscode.window.showInformationMessage(`已经执行了以下命令：${commands.slice(0, i).map(cmd => cmd.name).join(', ')}`);
+					vscode.window.showWarningMessage(`以下命令没有执行：${commands.slice(i).map(cmd => cmd.name).join(', ')}`);
+					reject(error);
+					return;
+				}
+			}
+			resolve();
+		});
+
+	});
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	let create_cmd = vscode.commands.registerCommand('cmdterminalmaker.createTerminal', async () => {
+		const commandConfigs: (Command | undefined)[] = vscode.workspace.getConfiguration('cmdterminalmaker').get<Command[]>('commands') ?? [];
+		const commands = commandConfigs
+			.map((config) => {
+				const { name, command, terminalName, parameter } = config as Command;
+				return new Command(name, command, terminalName, parameter);
+			});
+		const name2commands: Map<string, Command> = new Map(commands.map(command => [command.name, command]));
+		const chosenCmmandNames = await vscode.window.showQuickPick(commands.map(command => command.name), {
+			placeHolder: '请选择命令进行执行',
+			canPickMany: true,
+			matchOnDescription: true
+		});
+		await executeCommands(chosenCmmandNames?.map(name => name2commands.get(name) as Command) ?? []);
+	});
+	context.subscriptions.push(create_cmd);
+}
+
+export function deactivate() { }
